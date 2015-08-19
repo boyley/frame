d457dff9a24830dae5d21a07dfed4ed5c15af7fa
Boyley <274163579@qq.com>
2015-08-19 15:56:02 +0800

    angulr整理

--- standard/cms/src/main/resources/static/scripts/components/form/pager.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('app')
-    .directive('appPager', function() {
-        return {
-            templateUrl: 'scripts/components/form/pager.html'
-        };
-    });
